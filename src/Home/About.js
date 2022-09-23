48c11544233f61b355e748390202ebbeb98cffdd
sandeepflutter <sandeepbudha.sb@gmail.com>
2022-09-23 16:54:36 +0545

    icon+

--- src/Home/About.js
+++ src/Home/About.js
@@ -2,6 +2,27 @@ import React from 'react';
 import '../App.css';
 import 'aos/dist/aos.css'
 import {FaBirthdayCake} from 'react-icons/fa';
+import {FaGlassCheers} from 'react-icons/fa';
+import {FaUsers} from 'react-icons/fa';
+import {FaWolfPackBattalion} from 'react-icons/fa';
+import {FaGratipay} from 'react-icons/fa';
+import {FaDove} from 'react-icons/fa';
+import {FaRegSnowflake} from 'react-icons/fa';
+import {FaMugHot} from 'react-icons/fa';
+import {FaFireAlt} from 'react-icons/fa';
+
+import {FaBook} from 'react-icons/fa';
+import {FaCrown} from 'react-icons/fa';
+import {FaLeaf} from 'react-icons/fa';
+import {FaAtom} from 'react-icons/fa';
+import {FaBriefcase} from 'react-icons/fa';
+import {FaFistRaised} from 'react-icons/fa';
+
+import {FaPeopleCarry} from 'react-icons/fa';
+import {FaMusic} from 'react-icons/fa';
+import {FaDog} from 'react-icons/fa';
+
+
 
 export const About  =  () => {     
 
@@ -9,7 +30,7 @@ export const About  =  () => {
 
     return (
       
-<div id="about" class=" bg-green-300 body-font">
+<div id="about" class=" bg-green-400 body-font">
 <section  class="px-5 lg:px-[9px] pl-5 pr-5">
    <div class="px-3 lg:px-20 sm:px-10 md:px-5 lg:flex md:flex">  
 <div className='lg:w-[200vh] md:w-[200vh]'>
@@ -26,7 +47,7 @@ export const About  =  () => {
 
 <div className='px-5 '>
 <div class="grid lg:grid-cols-4 md:grid-cols-4 grid-cols-2 gap-4">
-<div className="p-8 border-2 border-solid rounded-lg border-gray-500">
+<div className="p-8 border-2 border-solid rounded-lg border-gray-500 bg-white">
           <div className="max-w-md text-center">
             <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 sm:w-16 sm:h-16">
             <FaBirthdayCake className='w-full h-full  text-gray-500'/>
@@ -34,101 +55,141 @@ export const About  =  () => {
             <h6 className="mb-2 font-semibold leading-5 font-crimson">BIRTHDAY</h6>
           </div>
   </div>
-  <div className="p-8 border-2 border-solid rounded-lg border-gray-500">
+  <div className="p-8 border-2 border-solid rounded-lg border-gray-500 bg-white">
           <div className="max-w-md text-center">
             <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 sm:w-16 sm:h-16">
-            <FaBirthdayCake className='w-full h-full  text-gray-500'/>
+            <FaGlassCheers className='w-full h-full  text-gray-500'/>
             </div>
-            <h6 className="mb-2 font-semibold leading-5 font-crimson">BIRTHDAY</h6>
+            <h6 className="mb-2 font-semibold leading-5 font-crimson">NEW YEAR</h6>
           </div>
   </div>
-  <div className="p-8 border-2 border-solid rounded-lg border-gray-500">
+  <div className="p-8 border-2 border-solid rounded-lg border-gray-500 bg-white">
           <div className="max-w-md text-center">
             <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 sm:w-16 sm:h-16">
-            <FaBirthdayCake className='w-full h-full  text-gray-500'/>
+            <FaUsers className='w-full h-full  text-gray-500'/>
             </div>
-            <h6 className="mb-2 font-semibold leading-5 font-crimson">BIRTHDAY</h6>
+            <h6 className="mb-2 font-semibold leading-5 font-crimson">INSPIRATIONAL</h6>
           </div>
   </div>
-  <div className="p-8 border-2 border-solid rounded-lg border-gray-500">
+  <div className="p-8 border-2 border-solid rounded-lg border-gray-500 bg-white">
           <div className="max-w-md text-center">
             <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 sm:w-16 sm:h-16">
-            <FaBirthdayCake className='w-full h-full  text-gray-500'/>
+            <FaWolfPackBattalion className='w-full h-full  text-gray-500'/>
             </div>
-            <h6 className="mb-2 font-semibold leading-5 font-crimson">BIRTHDAY</h6>
+            <h6 className="mb-2 font-semibold leading-5 font-crimson">ATTITUDE</h6>
           </div>
   </div>
-  <div className="p-8 border-2 border-solid rounded-lg border-gray-500">
+  <div className="p-8 border-2 border-solid rounded-lg border-gray-500 bg-white">
           <div className="max-w-md text-center">
             <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 sm:w-16 sm:h-16">
-            <FaBirthdayCake className='w-full h-full  text-gray-500'/>
+            <FaGratipay className='w-full h-full  text-gray-500'/>
             </div>
-            <h6 className="mb-2 font-semibold leading-5 font-crimson">BIRTHDAY</h6>
+            <h6 className="mb-2 font-semibold leading-5 font-crimson">LOVE</h6>
           </div>
   </div>
-  <div className="p-8 border-2 border-solid rounded-lg border-gray-500">
+  <div className="p-8 border-2 border-solid rounded-lg border-gray-500 bg-white">
           <div className="max-w-md text-center">
             <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 sm:w-16 sm:h-16">
-            <FaBirthdayCake className='w-full h-full  text-gray-500'/>
+            <FaDove className='w-full h-full  text-gray-500'/>
             </div>
-            <h6 className="mb-2 font-semibold leading-5 font-crimson">BIRTHDAY</h6>
+            <h6 className="mb-2 font-semibold leading-5 font-crimson">RELIGION</h6>
           </div>
   </div>
-  <div className="p-8 border-2 border-solid rounded-lg border-gray-500">
+  <div className="p-8 border-2 border-solid rounded-lg border-gray-500 bg-white">
           <div className="max-w-md text-center">
             <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 sm:w-16 sm:h-16">
-            <FaBirthdayCake className='w-full h-full  text-gray-500'/>
+            <FaRegSnowflake className='w-full h-full  text-gray-500'/>
             </div>
-            <h6 className="mb-2 font-semibold leading-5 font-crimson">BIRTHDAY</h6>
+            <h6 className="mb-2 font-semibold leading-5 font-crimson">CREATIVITY</h6>
           </div>
   </div>
 
-  <div className="p-8 border-2 border-solid rounded-lg border-gray-500">
+  <div className="p-8 border-2 border-solid rounded-lg border-gray-500 bg-white">
           <div className="max-w-md text-center">
             <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 sm:w-16 sm:h-16">
-            <FaBirthdayCake className='w-full h-full  text-gray-500'/>
+            <FaMugHot className='w-full h-full  text-gray-500'/>
             </div>
-            <h6 className="mb-2 font-semibold leading-5 font-crimson">BIRTHDAY</h6>
+            <h6 className="mb-2 font-semibold leading-5 font-crimson">LIFE</h6>
           </div>
   </div>
-  <div className="p-8 border-2 border-solid rounded-lg border-gray-500">
+  <div className="p-8 border-2 border-solid rounded-lg border-gray-500 bg-white">
           <div className="max-w-md text-center">
             <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 sm:w-16 sm:h-16">
-            <FaBirthdayCake className='w-full h-full  text-gray-500'/>
+            <FaFireAlt className='w-full h-full  text-gray-500'/>
             </div>
-            <h6 className="mb-2 font-semibold leading-5 font-crimson">BIRTHDAY</h6>
+            <h6 className="mb-2 font-semibold leading-5 font-crimson">PASSION</h6>
           </div>
   </div>
-  <div className="p-8 border-2 border-solid rounded-lg border-gray-500">
+  <div className="p-8 border-2 border-solid rounded-lg border-gray-500 bg-white">
           <div className="max-w-md text-center">
             <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 sm:w-16 sm:h-16">
-            <FaBirthdayCake className='w-full h-full  text-gray-500'/>
+            <FaBook className='w-full h-full  text-gray-500'/>
             </div>
-            <h6 className="mb-2 font-semibold leading-5 font-crimson">BIRTHDAY</h6>
+            <h6 className="mb-2 font-semibold leading-5 font-crimson">PROVERBS</h6>
           </div>
   </div>
-  <div className="p-8 border-2 border-solid rounded-lg border-gray-500">
+  <div className="p-8 border-2 border-solid rounded-lg border-gray-500 bg-white">
           <div className="max-w-md text-center">
             <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 sm:w-16 sm:h-16">
-            <FaBirthdayCake className='w-full h-full  text-gray-500'/>
+            <FaLeaf className='w-full h-full  text-gray-500'/>
             </div>
-            <h6 className="mb-2 font-semibold leading-5 font-crimson">BIRTHDAY</h6>
+            <h6 className="mb-2 font-semibold leading-5 font-crimson">NATURE</h6>
           </div>
   </div>
-  <div className="p-8 border-2 border-solid rounded-lg border-gray-500">
+  <div className="p-8 border-2 border-solid rounded-lg border-gray-500 bg-white">
           <div className="max-w-md text-center">
             <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 sm:w-16 sm:h-16">
-            <FaBirthdayCake className='w-full h-full  text-gray-500'/>
+            <FaDog className='w-full h-full  text-gray-500'/>
             </div>
-            <h6 className="mb-2 font-semibold leading-5 font-crimson">BIRTHDAY</h6>
+            <h6 className="mb-2 font-semibold leading-5 font-crimson">PET</h6>
           </div>
   </div>
-  <div className="p-8 border-2 border-solid rounded-lg border-gray-500">
+  <div className="p-8 border-2 border-solid rounded-lg border-gray-500 bg-white">
           <div className="max-w-md text-center">
             <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 sm:w-16 sm:h-16">
-            <FaBirthdayCake className='w-full h-full  text-gray-500'/>
+            <FaMusic className='w-full h-full  text-gray-500'/>
             </div>
-            <h6 className="mb-2 font-semibold leading-5 font-crimson">BIRTHDAY</h6>
+            <h6 className="mb-2 font-semibold leading-5 font-crimson">MUSIC</h6>
+          </div>
+  </div>
+  <div className="p-8 border-2 border-solid rounded-lg border-gray-500 bg-white">
+          <div className="max-w-md text-center">
+            <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 sm:w-16 sm:h-16">
+            <FaFistRaised className='w-full h-full  text-gray-500'/>
+            </div>
+            <h6 className="mb-2 font-semibold leading-5 font-crimson">MOVIE</h6>
+          </div>
+  </div>
+  <div className="p-8 border-2 border-solid rounded-lg border-gray-500 bg-white">
+          <div className="max-w-md text-center">
+            <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 sm:w-16 sm:h-16">
+            <FaPeopleCarry className='w-full h-full  text-gray-500'/>
+            </div>
+            <h6 className="mb-2 font-semibold leading-5 font-crimson">FRIENDSHIP</h6>
+          </div>
+  </div>
+  <div className="p-8 border-2 border-solid rounded-lg border-gray-500 bg-white">
+          <div className="max-w-md text-center">
+            <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 sm:w-16 sm:h-16">
+            <FaBriefcase className='w-full h-full  text-gray-500'/>
+            </div>
+            <h6 className="mb-2 font-semibold leading-5 font-crimson">ENTREPRENEURSHIP</h6>
+          </div>
+  </div>
+  <div className="p-8 border-2 border-solid rounded-lg border-gray-500 bg-white">
+          <div className="max-w-md text-center">
+            <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 sm:w-16 sm:h-16">
+            <FaAtom className='w-full h-full  text-gray-500'/>
+            </div>
+            <h6 className="mb-2 font-semibold leading-5 font-crimson">SCIENCE</h6>
+          </div>
+  </div>
+  <div className="p-8 border-2 border-solid rounded-lg border-gray-500 bg-white">
+          <div className="max-w-md text-center">
+            <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 sm:w-16 sm:h-16">
+            <FaCrown className='w-full h-full  text-gray-500'/>
+            </div>
+            <h6 className="mb-2 font-semibold leading-5 font-crimson">FAMOUS PEOPLE</h6>
           </div>
   </div>
 </div>
